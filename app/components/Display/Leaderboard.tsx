5dbee123a496cad41874a42ef509577677bbea96
Arthur <arthur@DESKTOP-C88PO09.localdomain>
2025-08-01 22:47:28 -0300

    feat: leaderboard infinite scrolling

--- app/components/Display/Leaderboard.tsx
+++ app/components/Display/Leaderboard.tsx
@@ -1,40 +1,93 @@
 "use client"
 
 import { useEffect, useRef, useState } from 'react'
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer'
 
 import { RuleInfo } from '@/app/utils/utils'
 import { getOutputs, VerificationOutput, VerifyPayloadProxyInput } from '@/app/utils/backend-libs/core/lib';
 import { DecodedIndexerOutput } from '@/app/utils/backend-libs/cartesapp/lib';
 
 const RIVES_NODE_URL = process.env.NEXT_PUBLIC_RIVES_NODE_URL;
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 15;
 
 function Leaderboard({ruleInfo}:{ruleInfo:RuleInfo}) {
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const [rankingFocus, setRankingFocus] = useState(0);
+    const [rankingFocus, setRankingFocus] = useState(1);
+    const totalEntries = useRef(1);
+    const isFetchingRef = useRef(true); // Ref used to prevent scrolling the leaderboard while fetching
+    const [ prevRankingFocusRef, prevRankingFocusInView ] = useInView();
+    const [ nextRankingFocusRef, nextRankingFocusInView ] = useInView();
     const isMovingRef = useRef(false); // <-- Ref used for timing logic
+    const scrollRef = useRef<HTMLDivElement>(null);
+    const { ref, inView } = useInView();
 
-    const { isPending, error, data, isFetching } = useQuery({
-        queryKey: ['leaderboardData', page],
-        queryFn: async () => {
+    const {
+        status,
+        data,
+        error,
+        isFetching,
+        isFetchingNextPage,
+        fetchNextPage,
+        hasNextPage,
+    } = useInfiniteQuery({
+        queryKey: ['leaderboardData'],
+        queryFn: async ({
+        pageParam,
+        }): Promise<{
+        data:VerificationOutput[]
+        nextPage: number|undefined
+        }> => {
             const tags = ["score", ruleInfo.cartridge_id, ruleInfo.id];
             const response:DecodedIndexerOutput = await getOutputs(
             {
                 tags,
                 type: 'notice',
-                page,
+                page: pageParam,
                 page_size: PAGE_SIZE,
                 order_by: "value",
                 order_dir: "desc"
             },
             {cartesiNodeUrl: RIVES_NODE_URL});
+            const total_pages = Math.ceil(response.total / PAGE_SIZE);
+            totalEntries.current = response.total;
 
-            setTotalPages(response.total);
-            return response.data as VerificationOutput[];
+            return {data: response.data as VerificationOutput[], nextPage: pageParam+1 <= total_pages ? pageParam+1 : undefined};
         },
-    });
+        initialPageParam: 1,
+        getNextPageParam: (lastPage) => lastPage.nextPage,
+    })
+
+    useEffect(() => {
+        if (inView && hasNextPage) {
+            fetchNextPage()
+        }
+    }, [fetchNextPage, inView])
+
+    useEffect(() => {
+        const scrollEl = scrollRef.current;
+        if (!scrollEl) return;
+        const scrollAmount = 32;
+
+        // if next row is not in view? scroll down
+        // else if previous row is not in view? scroll up
+        if (!nextRankingFocusInView) {
+            if (rankingFocus+1 == totalEntries.current) {
+                scrollEl.scrollBy({ top: +scrollAmount*2, behavior: 'smooth' });
+            } else {
+                scrollEl.scrollBy({ top: +scrollAmount, behavior: 'smooth' });
+            }
+        } else if (!prevRankingFocusInView) {
+            if (rankingFocus-1 == 1) {
+                scrollEl.scrollBy({ top: -scrollAmount*3, behavior: 'smooth' });
+            } else {
+                scrollEl.scrollBy({ top: -scrollAmount, behavior: 'smooth' });
+            }
+        }
+    }, [prevRankingFocusRef, nextRankingFocusRef, nextRankingFocusInView, prevRankingFocusInView])
+
+    useEffect(() => {
+        isFetchingRef.current = isFetching;
+    }, [isFetching])
 
     useEffect(() => {
         const handleKeyDown = (e: KeyboardEvent) => {
@@ -46,10 +99,10 @@ function Leaderboard({ruleInfo}:{ruleInfo:RuleInfo}) {
             };
 
             if (e.key == "ArrowUp") {
-                setRankingFocus((prev) => Math.max(prev - 1, 0));
+                setRankingFocus((prev) => Math.max(prev - 1, 1));
                 delay_keydown_events();
-            } else if (e.key === "ArrowDown") {
-                setRankingFocus((prev) => prev+1);
+            } else if (e.key === "ArrowDown" && !isFetchingRef.current) {
+                setRankingFocus((prev) => Math.min(prev + 1, totalEntries.current));
                 delay_keydown_events();
             }
         };
@@ -63,7 +116,7 @@ function Leaderboard({ruleInfo}:{ruleInfo:RuleInfo}) {
     }, []);
 
 
-    if (isFetching || isPending) {
+    if (isFetching && !isFetchingNextPage) {
         return (
             <div className='emulator-screen bg-black text-white p-4 overflow-scroll relative'>
                 <div className='flex justify-center items-center h-full'>
@@ -82,27 +135,38 @@ function Leaderboard({ruleInfo}:{ruleInfo:RuleInfo}) {
             </div>
         );
     }
-    
+
     return (
-        <div className='emulator-screen bg-black text-white p-4 overflow-scroll relative'>
+        <div ref={scrollRef} className='emulator-screen bg-black text-white p-4 overflow-scroll relative'>
             <div>
                 <h2 className={`pixelated-font text-xl mb-2`}>{ruleInfo.name} Leaderboard</h2>
             </div>
 
             <div>
                 <div className='flex flex-col min-w-0'>
-                    {data.map((output, index) => (
-                        <div key={`${output._msgSender}+${output._timestamp}`}
-                         className={`flex justify-between p-2 ${rankingFocus === index ? 'bg-rives-purple' : ''}`} 
-                        >
-                            <span className='text-xs'>{index+1}</span>
-                            <span className='text-xs'>
-                                {output.user_address?.substring(0, 6)}...{output.user_address?.substring(output.user_address.length - 4)}
-                            </span>
-                            <span className='text-xs'>{output._timestamp}</span>
-                            <span className='text-xs'>{output.score.toString()}</span>
-                        </div>
+                    {data.pages.map((page, index) => (
+                        page.data.map((output, index2) => {
+                            const currentRanking = index * PAGE_SIZE + index2 + 1;
+                            return (
+                                <div 
+                                ref={currentRanking === rankingFocus+1? nextRankingFocusRef: currentRanking === rankingFocus-1? prevRankingFocusRef:undefined} 
+                                key={`${output._msgSender}+${output._timestamp}`}
+                                className={`flex justify-between p-2 ${rankingFocus === currentRanking ? 'bg-rives-purple' : ''}`} 
+                                >
+                                    <span className='text-xs'>{currentRanking}</span>
+                                    <span className='text-xs'>
+                                        {output.user_address?.substring(0, 6)}...{output.user_address?.substring(output.user_address.length - 4)}
+                                    </span>
+                                    <span className='text-xs'>{output._timestamp}</span>
+                                    <span className='text-xs'>{output.score.toString()}</span>
+                                </div>
+                            )
+                        })
                     ))}
+
+                    <div ref={ref} className='h-3'>
+                        <span className={`text-xs ${!isFetchingNextPage? "hidden": ""}`}>Loading...</span>
+                    </div>
                 </div>
             </div>
 
